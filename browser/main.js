e55693474510152c35efe3c8dd8bc0056212547b
James Halliday <mail@substack.net>
2014-08-02 00:20:28 +1200

    jumping

--- browser/main.js
+++ browser/main.js
@@ -11,27 +11,41 @@ xhr({ uri: '/game.svg' }, function (err, res, body) {
     player.appendChild(gplayer);
     gparent.appendChild(player);
     
-    var delta = { x: 0, y: 0 };
+    var accel = { x: 0, y: 0 };
+    var velo = { x: 0, y: 0 };
     var pos = { x: 0, y: 0 };
     
     window.addEventListener('keydown', function (ev) {
         var name = ev.keyIdentifier;
         if (name === 'Right') {
-            delta.x = -1;
+            velo.x = 1;
         }
         else if (name === 'Left') {
-            delta.x = 1;
+            velo.x = -1;
+        }
+        else if (ev.which === 32 && velo.y === 0) {
+            velo.y = 1;
+            accel.y = -1;
         }
     });
     
     var last = Date.now();
     (function tick () {
         var now = Date.now();
-        var dt = last - now;
+        var dt = now - last;
         last = now;
         
-        pos.x += delta.x * dt / 5;
+        velo.y += accel.y * dt / 200;
+        
+        pos.x += velo.x * dt / 3;
         pos.x = Math.min(200, Math.max(-375, pos.x));
+        pos.y -= velo.y * dt * 1.2;
+        
+        if (pos.y > 0) {
+            velo.y = 0;
+            accel.y = 0;
+            pos.y = 0;
+        }
         
         var tr = pos.x + ',' + pos.y;
         player.setAttribute('transform', 'translate(' + tr + ')');
