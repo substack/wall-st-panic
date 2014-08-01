a0d6a12a7f7c0cf431006752047143f7c102f584
James Halliday <mail@substack.net>
2014-08-02 00:53:56 +1200

    raining money

--- browser/main.js
+++ browser/main.js
@@ -1,10 +1,20 @@
 var xhr = require('xhr');
-var root = document.querySelector('#root');
 var createPlayer = require('./player.js');
+var createMoney = require('./money.js');
+
+var root = document.querySelector('#root');
 
 xhr({ uri: '/game.svg' }, function (err, res, body) {
     root.innerHTML = body;
     var player = createPlayer(root.querySelector('svg #player'));
+    
+    var emoney = root.querySelector('svg #money');
+    emoney.parentNode.removeChild(emoney);
+    
+    var money = createMoney(emoney);
+    money.appendTo(root.querySelector('svg'));
+    
+    setInterval(function () { money.rain() }, 1000);
     
     window.addEventListener('keydown', function (ev) {
         var name = ev.keyIdentifier;
@@ -26,6 +36,7 @@ xhr({ uri: '/game.svg' }, function (err, res, body) {
         last = now;
         
         player.tick(dt);
+        money.tick(dt);
         
         setTimeout(function () {
             window.requestAnimationFrame(tick);
