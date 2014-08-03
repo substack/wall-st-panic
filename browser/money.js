18766a1e585df9b00b45c3de1faee035987a809f
James Halliday <mail@substack.net>
2014-08-03 19:49:24 +1200

    using box-sprite-svg for money

--- browser/money.js
+++ browser/money.js
@@ -1,34 +1,16 @@
-var inherits = require('inherits');
-var EventEmitter = require('events').EventEmitter;
-var createElement = require('svg-create-element');
+var Sprite = require('box-sprite-svg');
 
-module.exports = Money;
-inherits(Money, EventEmitter);
-
-function Money (elem, cash) {
-    if (!(this instanceof Money)) return new Money(elem, cash);
-    EventEmitter.call(this);
+module.exports = function (elem, cash) {
+    var sp = Sprite(elem);
+    sp.position = { x: Math.random() * 595 - 485, y: -350 };
+    sp.velocity.y = 200;
     
-    this.element = createElement('g');
-    this.element.appendChild(elem);
+    sp.on('tick', function () {
+        if (sp.position.y > 100) sp.emit('miss');
+    });
     
-    this.pos = { x: Math.random() * 595 - 485, y: -350 };
-    this.v = { x: 0, y: 1 };
-    this.cash = cash;
-    this.tick(0);
-}
-
-Money.prototype.appendTo = function (target) {
-    target.appendChild(this.element);
-};
-
-Money.prototype.tick = function (dt) {
-    this.pos.y += dt * this.v.y / 5;
-    var tr = this.pos.x + ',' + this.pos.y;
-    this.element.setAttribute('transform', 'translate(' + tr + ')');
-    if (this.pos.y > 100) this.emit('miss');
-};
-
-Money.prototype.bbox = function () {
-    return this.element.getBoundingClientRect();
+    sp.cash = cash;
+    sp.tick(0);
+    
+    return sp;
 };
